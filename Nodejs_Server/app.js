843ccfa90f708a743799a324bfeb149c07405e87
Sarvesh207 <sarveshgaynar@gmail.com>
2023-10-21 23:34:55 +0530

    started backend practice

---
+++ Nodejs_Server/app.js
@@ -0,0 +1,32 @@
+// const express = require('express')
+
+
+// const app = express();
+
+// app.listen(3000, () => {
+//     console.log('Server is running on port 3000')
+
+// })
+
+// app.get('/', (req, res) => {
+//     res.sendFile('./views/index.html', {root:__dirname})
+// })
+
+// app.get('/about', (req, res) => {
+//     res.sendFile('./views/about.html', {root:__dirname})
+// })
+
+// //redirect
+
+// app.get('/about-us', (req, res) => {
+//     res.redirect('/about')
+// })
+
+
+// //404 Page
+
+// app.use((req, res) => {
+//     res.status(404).sendFile('./views/404.html', {root:__dirname})
+// })
+
+// //Post -> to send data from frontend to backend
